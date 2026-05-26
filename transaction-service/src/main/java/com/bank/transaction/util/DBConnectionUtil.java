d0412e36ffa738de699a198180dcd74c3af64dcb
Abhishek Jachak <abhishekjachak22@gmail.com>
2026-05-26 22:57:44 +0530

    jdbc procedures updated

--- transaction-service/src/main/java/com/bank/transaction/util/DBConnectionUtil.java
+++ transaction-service/src/main/java/com/bank/transaction/util/DBConnectionUtil.java
@@ -9,8 +9,7 @@ public class DBConnectionUtil {
     private static final String URL =
             "jdbc:postgresql://localhost:5432/bank_db";
 
-    private static final String USERNAME =
-            "postgres";
+    private static final String USERNAME = "postgres";
 
     private static final String PASSWORD =
             "root123";
